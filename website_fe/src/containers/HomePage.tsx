e054dad78e7f5e46403d83b02cdcda0a60c8189f
Jerica Tolentino <jericatolentino@Jericas-MacBook-Pro.local>
2022-04-01 20:57:44 +0100

    Style exapandable cards better

--- website_fe/src/containers/HomePage.tsx
+++ website_fe/src/containers/HomePage.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import Card from '../components/ExpandableCard';
+import styled from 'styled-components'
+
+const HomePageDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+
+const HomePageCard = styled.div`
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+`
 
 const HomePage: React.VFC<{}> = () => {
   return (
-    <><Card value={'blahdfidfoweifhwei'}/></>
+    <HomePageDiv>
+        {['jerica', 'luke', 'jereni'].map(person => 
+            <HomePageCard>
+                <Card value={person}/>
+            </HomePageCard>
+        )}
+    </HomePageDiv>
   );
 }
 
